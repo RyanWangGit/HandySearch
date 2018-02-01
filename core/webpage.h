392a9cd71ebbf6329198c9c2e6a54500b961564f
Ryan Wang <ryanwang.cs@gmail.com>
2018-02-01 00:07:39 -0500

    Implement Webpage.

--- core/webpage.h
+++ core/webpage.h
@@ -1,7 +1,16 @@
 #pragma once
 
+#include <QString>
+
 class Webpage
 {
+private:
+    QString title;
+    QString content;
+    QString url;
 public:
-    Webpage();
+    Webpage(const QString &title, const QString &content, const QString &url);
+    inline const QString &getTitle() { return this->title; }
+    inline const QString &getContent() { return this->content; }
+    inline const QString &getUrl() { return this->url; }
 };
