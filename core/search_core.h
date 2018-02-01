898e9e9496ee2016b4dc52167eef93776aa10be8
Ryan Wang <ryanwang.cs@gmail.com>
2018-02-01 00:02:24 -0500

    Add private members and public interfaces.

--- core/search_core.h
+++ core/search_core.h
@@ -1,9 +1,26 @@
 #pragma once
 
+#include <QString>
+#include <QStringList>
+#include <QList>
+#include <QSharedPointer>
+#include "webpage.h"
+#include "dictionary.h"
+#include "inverted_list.h"
+
+typedef struct SearchResult {
+    QStringList keywords;
+    QList<Webpage *> webpages;
+}SearchResult;
 
 class SearchCore
 {
+private:
+    Dictionary dictionary;
+    InvertedList invertedList;
+    bool hasLoaded;
 public:
-    SearchCore();
-
+    SearchCore(const QString &dictionary, const QString &database);
+    void load();
+    QSharedPointer<SearchResult> query(const QString &sentence);
 };
