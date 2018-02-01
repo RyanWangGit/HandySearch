d9bd29c358bbe82bfaaf1e065380d931fa29ed7a
Ryan Wang <ryanwang.cs@gmail.com>
2018-02-01 00:02:24 -0500

    Add private members and public interfaces.

--- core/search_core.cpp
+++ core/search_core.cpp
@@ -1,6 +1,23 @@
 #include "search_core.h"
 
-SearchCore::SearchCore()
+SearchCore::SearchCore(const QString &dictionary, const QString &database)
 {
+    this->hasLoaded = false;
+}
 
+
+
+QSharedPointer<SearchResult> SearchCore::query(const QString &sentence)
+{
+    if(!this->hasLoaded)
+        qFatal("Core hasn't loaded anything yet.");
+
+    QSharedPointer<SearchResult> result(new SearchResult());
+
+    return result;
+}
+
+void SearchCore::load()
+{
+    this->hasLoaded = true;
 }
