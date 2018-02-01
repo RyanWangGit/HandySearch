2dc1a0e5e3002752966443d05a466b10d097a15a
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-02-01 00:07:39 -0500

    Implement Webpage.

--- core/webpage.cpp
+++ core/webpage.cpp
@@ -1,6 +1,8 @@
 #include "webpage.h"
 
-Webpage::Webpage()
+Webpage::Webpage(const QString &title, const QString &content, const QString &url)
 {
-
+    this->title = title;
+    this->content = content;
+    this->url = url;
 }
