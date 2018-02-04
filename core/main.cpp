3e6c84b572f9d89469d91a08a22408232af1b2b7
Ryan Wang <ryanwang.cs@gmail.com>
2018-02-03 19:28:31 -0500

    Register the QList<Webpage*> type for signals.

--- core/main.cpp
+++ core/main.cpp
@@ -1,18 +1,3 @@
-/*************************************
- * Copyright(C),2015-2016,Ryan Wang 
- * 
- * File:    main.cpp
- *
- * Version: V1.0
- * 
- * Brief:    This is the top-layer of the application which
- * creates main window of QT and start the application's main
- * event loop.
- *
- * Author:    Ryan
- 
- * Date:    Oct. 2015
-*************************************/
 #include "stdafx.h"
 #include "handy_search.h"
 
@@ -21,6 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     qRegisterMetaType<QList<Html*>>("QList<Html*>");
+    qRegisterMetaType<QList<Webpage*>>("QList<Webpage*>");
     QTextCodec::setCodecForLocale(QTextCodec::codecForName("utf-8"));
 
     HandySearch w;
