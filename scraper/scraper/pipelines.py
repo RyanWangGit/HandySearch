6e878d3935beb3ce7d7025d46aa3c5b7277e45f7
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-03-05 19:59:22 -0500

    Filter out more meaningless webpages.

--- scraper/scraper/pipelines.py
+++ scraper/scraper/pipelines.py
@@ -3,7 +3,6 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/topics/item-pipeline.html
 
-import sqlite3
 from scrapy.exceptions import DropItem
 
 
@@ -13,5 +12,11 @@ class ValidationPipeline:
             raise DropItem('Item is None')
         elif None or '' in item.values():
             raise DropItem('Item field not complete')
+        elif item['title'].startswith("创建“") and item['title'].endswith("” - 维基百科，自由的百科全书"):
+            raise DropItem('Item title contains 创建')
+        elif item['title'].startswith('维基百科:'):
+            raise DropItem('Item title contains 维基百科:')
+        elif 'Category:' in item['title']:
+            raise DropItem('Item title contains category:')
         else:
             return item
