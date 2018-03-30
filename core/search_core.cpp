9b1b9d008a4b109a99a20883ce5f8418b9aed21e
Ryan Wang <ryanwang.cs@gmail.com>
2018-03-29 21:42:45 -0400

    Create simple map-reduce concurrent framework.

--- core/search_core.cpp
+++ core/search_core.cpp
@@ -1,3 +1,8 @@
+#include "stable.h"
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QDebug>
+#include <QtConcurrent>
 #include "search_core.h"
 
 SearchCore::SearchCore(const QString &dictionary, const QString &database)
@@ -46,8 +51,65 @@ void SearchCore::query(const QString &sentence)
     return;
 }
 
+static QString _databasePath;
 
-void SearchCore::load()
+QMultiHash<QString, Index *> mapper(const QPair<int, int> &task)
+{
+    // open a thread-specific database connection
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE", QUuid::createUuid().toString());
+    db.setDatabaseName(_databasePath);
+    if(!db.open())
+        qFatal("Database cannot be opened.");
+
+    QSqlQuery query(db);
+    query.prepare("SELECT ROWID, content from `webpages` WHERE ROWID >= :start AND ROWID <= :end");
+    query.bindValue(":start", task.first);
+    query.bindValue(":end", task.second);
+    query.exec();
+
+    QMultiHash<QString, Index *> result;
+    while (!query.next()) {
+        // TODO: create Index and insert into result hashmap.
+    }
+    db.close();
+    return result;
+}
+
+
+QMultiHash<QString, Index *> &reducer(QMultiHash<QString, Index *> &result, const QMultiHash<QString, Index *> &other)
+{
+    result.unite(other);
+    return result;
+}
+
+
+void SearchCore::load(int from)
 {
     this->hasLoaded = true;
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE", QUuid::createUuid().toString());
+    db.setDatabaseName(this->databasePath);
+    if(!db.open())
+        qFatal("Database cannot be opened.");
+    QSqlQuery query("SELECT COUNT(id) FROM `webpages`", db);
+    if(!query.next())
+        qFatal("Database execution failed.");
+
+    _databasePath = this->databasePath;
+
+    const int TOTAL_WEBPAGES = query.value(0).toInt();
+    const int WEBPAGES_PER_THREAD = float(TOTAL_WEBPAGES - from + 1) / QThread::idealThreadCount();
+
+    db.close();
+
+    QList<QPair<int, int> > tasks;
+    for(int i = from; i < TOTAL_WEBPAGES; i += WEBPAGES_PER_THREAD)
+    {
+        if(i + WEBPAGES_PER_THREAD >= TOTAL_WEBPAGES)
+            tasks.append(QPair<int, int>(i, TOTAL_WEBPAGES));
+        else
+            tasks.append(QPair<int, int>(i, i + WEBPAGES_PER_THREAD - 1));
+    }
+
+    this->invertedList = QtConcurrent::blockingMappedReduced<QMultiHash<QString, Index *> >(tasks, mapper, reducer, QtConcurrent::UnorderedReduce);
+    qDebug() << this->invertedList;
 }
