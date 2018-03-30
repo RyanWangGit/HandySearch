dcbfe871ff0583564544cc8a5509b05dd25f13fa
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-03-29 21:42:45 -0400

    Create simple map-reduce concurrent framework.

--- core/search_core.h
+++ core/search_core.h
@@ -4,26 +4,28 @@
 #include <QStringList>
 #include <QList>
 #include <QObject>
+#include <QMultiHash>
 #include "webpage.h"
 #include "dictionary.h"
 #include "inverted_list.h"
+#include "index.h"
 
 class SearchCore : public QObject
 {
     Q_OBJECT
 private:
     Dictionary dictionary;
-    InvertedList invertedList;
     bool hasLoaded;
     QString dictionaryPath;
     QString databasePath;
+    QMultiHash<QString, Index *> invertedList;
 public:
     SearchCore(const QString &dictionary, const QString &database);
     SearchCore(const QString &database);
     SearchCore();
     void setPath(const QString &dictionary, const QString &database);
     void setPath(const QString &database);
-    void load();
+    void load(int from = 1);
     void query(const QString &sentence);
 signals:
     void result(const QStringList &keywords, const QList<Webpage *> &webpages);
