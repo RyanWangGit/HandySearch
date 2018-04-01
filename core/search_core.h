bb5e5f3d2e94391465d10a755d729a6ed8bcf88d
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-04-01 18:18:32 -0400

    Redesign the invertedlist.
    InvertedList: QHash<QString, QHash<int, QList<int> > >. Double hashmap maps from word to id and from id to position list.
    
    Mapper: QPair<int, int> -> QList<std::tuple<QString, int, int> >, from <start, end> task to <word, id, pos> result tuple.
    
    Reducer: std::tuple<QString, int, int> -> InvertedList. Store the result tuples into the inverted hash.

--- core/search_core.h
+++ core/search_core.h
@@ -4,7 +4,7 @@
 #include <QStringList>
 #include <QList>
 #include <QObject>
-#include <QMultiHash>
+#include <QHash>
 #include <QSqlDatabase>
 #include <QSharedPointer>
 #include "dictionary.h"
@@ -16,7 +16,7 @@ private:
     Dictionary dictionary;
     bool hasLoaded;
     QString dictionaryPath, databasePath;
-    QMultiHash<QString, QPair<int, QList<int> > > invertedList;
+    QHash<QString, QHash<int, QList<int> > > invertedList;
     QSqlDatabase db;
     unsigned int maxProgress;
 public:
