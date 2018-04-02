4534d50faec1e2bb260cddf2c3fec34f6ed9940f
Yuxin Wang <yuxinwang.dev@gmail.com>
2018-04-02 14:37:44 -0400

    Add signals to communicate with core.

--- ui/handy_search.h
+++ ui/handy_search.h
@@ -13,6 +13,11 @@ public:
     HandySearch(QWidget *parent = 0);
     void searchResult(const QStringList &keywords, const QList<Webpage> &webpages);
     bool load();
+signals:
+    void startLoading(int from=1);
+    void query(const QString &sentence);
+    void progress(const QString &hint, float progress);
+    void loadFinished();
 protected:
     void resizeEvent(QResizeEvent *event);
 private:
