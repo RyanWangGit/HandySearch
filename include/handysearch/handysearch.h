44e8be390ee83b83b9cf3646ffcf6d8292f4f70c
Ryan Wang <ryanwang.cs@gmail.com>
2018-12-26 22:26:28 -0500

    Add basic functions and empty implementations.

--- include/handysearch/handysearch.h
+++ include/handysearch/handysearch.h
@@ -1,9 +1,20 @@
 #pragma once
+#include <cstdint>
+#include <string>
 
 namespace handysearch {
 
 class search_core {
-
+public:
+  search_core();
+  search_core(const std::string &database);
+  search_core(const std::string &database, const std::string &dictionary_file);
+  search_core(search_core &other);
+  ~search_core();
+  search_core& operator=(search_core const &other);
+  void load_dictionary(const std::string &dictionary_file, void (*report_hook)(std::uint64_t current, std::uint64_t total) = nullptr);
+  void load_database(const std::string &database, void (*report_hook)(std::uint64_t current, std::uint64_t total) = nullptr);
+  std::uint64_t total_items() const;
 };
 
 }
--- src/handysearch.cpp
+++ src/handysearch.cpp
@@ -1,3 +1,44 @@
 #include <handysearch/handysearch.h>
+#include <cstdint>
+#include <string>
 
+
+std::uint64_t handysearch::search_core::total_items() const {
+  return 0;
+}
+
+handysearch::search_core::search_core() {
+
+}
+
+handysearch::search_core::search_core(const std::string &database) {
+
+}
+
+handysearch::search_core::search_core(const std::string &database, const std::string &dictionary_file) {
+
+}
+
+handysearch::search_core::search_core(handysearch::search_core &other) {
+
+}
+
+handysearch::search_core::~search_core() {
+
+}
+
+handysearch::search_core &handysearch::search_core::operator=(handysearch::search_core const &other) {
+  if (this != &other) {
+    
+  }
+  return *this;
+}
+
+void handysearch::search_core::load_dictionary(const std::string &dictionary_file, void (*report_hook)(std::uint64_t, std::uint64_t)) {
+
+}
+
+void handysearch::search_core::load_database(const std::string &database, void (*report_hook)(std::uint64_t, std::uint64_t)) {
+
+}
 
