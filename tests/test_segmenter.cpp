74819f33be1dea30b95268457dd74ca09e9ec281
Yuxin (Ryan) Wang <ryanwang.cs@gmail.com>
2019-04-20 00:51:48 -0400

    Add test for segmenter.

--- tests/test_segmenter.cpp
+++ tests/test_segmenter.cpp
@@ -1,4 +1,16 @@
-//
-// Created by Ryan Wang on 2019-04-20.
-//
+#include <iostream>
+#include <catch.hpp>
+#include <handysearch/segmenter.h>
+
+TEST_CASE("Test segmenter functionality", "[HandySearch::Segmenter]") {
+  handysearch::dictionary dictionary;
+  std::uint64_t test_current = 0, test_total = 0;
+  dictionary.load("./sample_dictionary.txt");
+
+  handysearch::segmenter segmenter(dictionary);
+  std::string test_string = "This is a test, 这是一个测试";
+  std::vector<std::string> result = segmenter.segment(test_string);
+  REQUIRE(result.size() > 0);
+}
+
 
