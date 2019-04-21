8e8b1b354080df56b1a51d9ac69a4a7354da5627
Yuxin (Ryan) Wang <ryanwang.cs@gmail.com>
2019-04-21 19:06:16 -0400

    Remove custom dictionary class.

--- include/handysearch/handysearch.h
+++ include/handysearch/handysearch.h
@@ -13,9 +13,13 @@ public:
   search_core(search_core &other);
   ~search_core();
   search_core& operator=(search_core const &other);
-  void load_dictionary(std::string const &dictionary_file, std::function< void(std::uint64_t, std::uint64_t) > report_hook = nullptr);
+  void load_dictionary(std::string const &dictionary_file);
   void load_database(std::string const &database, std::function< void(std::uint64_t, std::uint64_t) > report_hook = nullptr);
   std::uint64_t total_items() const;
+
+private:
+  class impl;
+  std::unique_ptr<impl> m_impl;
 };
 
 }
