dcd0f03f652591bcfbcf972cb653742c4052a3b6
Yuxin (Ryan) Wang <ryanwang.cs@gmail.com>
2019-07-24 17:30:45 -0400

    Move SYSTEM flag to library target_include_directories.

--- core/libs/cppjieba.cmake
+++ core/libs/cppjieba.cmake
@@ -1,5 +1,5 @@
 add_library(cppjieba INTERFACE)
 target_include_directories(cppjieba
-        INTERFACE
+        INTERFACE SYSTEM
         $<BUILD_INTERFACE:${CMAKE_CURRENT_LIST_DIR}/cppjieba/deps>
         $<BUILD_INTERFACE:${CMAKE_CURRENT_LIST_DIR}/cppjieba/include>)
